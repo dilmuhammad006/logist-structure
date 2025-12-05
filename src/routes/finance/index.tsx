7719800b99131d8bf8f2f94fd9f3528ccb0f95f0
Dilmuhammad Abdumalikov <abdumalikovdilmuhammad06@gmail.com>
2025-12-05 16:57:23 +0500

    input-search-added

--- src/routes/finance/index.tsx
+++ src/routes/finance/index.tsx
@@ -9,35 +9,53 @@ import {
 } from "@/components/ui/table";
 import { createFileRoute } from "@tanstack/react-router";
 import { users } from "./db";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+import useDebounce from "@/hooks/debounce";
 
 export const Route = createFileRoute("/finance/")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
+  const [search, setSearch] = useState("");
+  const debounced = useDebounce(search, 700);
+  const filtered = users.filter(
+    (el) =>
+      el.name.toLowerCase().includes(debounced.toLocaleLowerCase()) ||
+      el.job.toLowerCase().includes(debounced.toLocaleLowerCase())
+  );
   return (
-    <Table className="border">
-      <TableHeader>
-        <TableRow>
-          <TableHead>ID</TableHead>
-          <TableHead>Name</TableHead>
-          <TableHead>Age</TableHead>
-          <TableHead>Job</TableHead>
-          <TableHead>Salary</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {users.map((el) => (
-          <TableRow key={el.id}>
-            <TableCell>{el.id}</TableCell>
-            <TableCell>{el.name}</TableCell>
-            <TableCell>{el.age}</TableCell>
-            <TableCell>{el.job}</TableCell>
-            <TableCell>{el.salary} $</TableCell>
+    <div className="w-full flex flex-col gap-4">
+      <Input
+        className="max-w-60"
+        type="search"
+        placeholder="Type here to seach..."
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <Table className="border">
+        <TableHeader>
+          <TableRow>
+            <TableHead>ID</TableHead>
+            <TableHead>Name</TableHead>
+            <TableHead>Age</TableHead>
+            <TableHead>Job</TableHead>
+            <TableHead>Salary</TableHead>
           </TableRow>
-        ))}
-      </TableBody>
-      <TableCaption>Users salary for a month in 2025</TableCaption>
-    </Table>
+        </TableHeader>
+        <TableBody>
+          {filtered.map((el) => (
+            <TableRow key={el.id}>
+              <TableCell>{el.id}</TableCell>
+              <TableCell>{el.name}</TableCell>
+              <TableCell>{el.age}</TableCell>
+              <TableCell>{el.job}</TableCell>
+              <TableCell>{el.salary} $</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+        <TableCaption>Employees salary for a month in 2025</TableCaption>
+      </Table>
+    </div>
   );
 }
